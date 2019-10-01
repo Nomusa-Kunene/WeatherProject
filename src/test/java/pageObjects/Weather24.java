a2b7d0f3557730db13f29b045aece47708b7b3ad
Nomusa-Kunene <55493243+Nomusa-Kunene@users.noreply.github.com>
2019-10-01 16:04:08 +0200

    page objects for weather24

--- src/test/java/pageObjects/Weather24.java
+++ src/test/java/pageObjects/Weather24.java
@@ -1,9 +1,11 @@
 package pageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,8 +18,6 @@ public class Weather24 {
     private WebElement btnGo;
     @FindBy(how = How.XPATH, using = "//*[@id=\"ext-gen32\"]")
     private WebElement weatherForecast;
-    private WebElement MinTemp;
-    private WebElement MaxTemp;
 
 
     public void selectCity(){
@@ -32,18 +32,7 @@ public class Weather24 {
         btnGo.click();
     }
 
-    public void clickExpandedForecast(){
+    public void clickExpandedForecast() {
         weatherForecast.click();
     }
-
-    public void MinTemp(){
-        String minTemperature = MinTemp.findElement(By.xpath(".//*[@id=\"forecastContent\"]/table/tbody/tr[3]/td[5]")).getText();
-        System.out.println("The minimum temperature is: " + minTemperature);
-    }
-
-    public void MaxTemp(){
-        String maxTemperature = MaxTemp.findElement(By.xpath(".//*[@id=\"forecastContent\"]/table/tbody/tr[3]/td[4]")).getText();
-        System.out.println("The maximum temperature is: " + maxTemperature);
-    }
-
 }
